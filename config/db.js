d45b1ac667e37a3b5ee6bb19f53ec5eb01651b91
pilotxd <rajendraveo@gmail.com>
2022-11-25 18:50:15 +0700

    KK4C

---
+++ config/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const mongoDB = async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/praktikumkk4c');
+    console.log('We Are Connected!')
+}
+
+module.exports = mongoDB;   
